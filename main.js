40b7a120d7f1b0ae72b25fc6740149f5eca0e264
SIDD12124 <72179881+SIDD12124@users.noreply.github.com>
2021-07-03 15:49:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+status = false;
+object = [];
+
+function setup(){
+    canvas = createCanvas(380,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    } else {
+        console.log(results);
+        object = results;
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+    
+    if(status != false){
+        r = random(255);
+        g = random(255);
+        b = random(255);
+    objectDetector.detect(video, gotResult);
+        for (var i = 0; i<object.length; i++) {
+            
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Numbers of objects detected are : " + object.length;
+
+            fill(r, g, b);
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 15);
+            noFill();
+            stroke(r, g, b);
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+    }
+}
